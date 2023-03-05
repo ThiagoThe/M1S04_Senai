93eeb1512b277b929654f0eb6457196ce4fa97ae
ThiagoThe <nitrox2thi@gmail.com>
2023-03-04 22:13:20 -0300

    add funcao de filtragem

--- app/js/models/listaNegociacoes.js
+++ app/js/models/listaNegociacoes.js
@@ -1,11 +1,23 @@
 export class listaNegociacoes {
-  #negociacoes = [];
+  constructor() {
+    this.negociacoes = [];
+  }
 
   adicionar(negociacao) {
-    this.#negociacoes.push(negociacao);
+    this.negociacoes.push(negociacao);
   }
 
   get negociacoes() {
-    return [].concat(this.#negociacoes);
+    return [].concat(this.negociacoes);
+  }
+
+  filtraNegociacoes(criterio) {
+    return this.negociacoes.filter(criterio);
   }
 }
+
+const criterio = (negociacao) => negociacao.valor > 5000;
+
+const listaDenegociacoes = new listaNegociacoes();
+
+const negociacoesFiltradas = listaDenegociacoes.filtraNegociacoes(criterio);
